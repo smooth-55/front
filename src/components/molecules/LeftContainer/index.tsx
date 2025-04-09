0983d618ac47ddd55c8dbcf956bfb936a73ee5e0
Ashish <dhakalashish18@gmail.com>
2025-04-09 18:19:56 +0545

    fix: repos

--- src/components/molecules/LeftContainer/index.tsx
+++ src/components/molecules/LeftContainer/index.tsx
@@ -1,9 +1,15 @@
+"use client";
 import { Repo } from "@/components/atoms";
 import { BookOutlined, CaretDownOutlined } from "@ant-design/icons";
 import { Button, Input } from "antd";
 import Image from "next/image";
+import { useMemo, useState } from "react";
 
 const LeftContainer = () => {
+  const [seeMore, setSeeMore] = useState(false);
+  const repoCount = useMemo(() => {
+    return seeMore ? 100 : 7;
+  }, [seeMore]);
   return (
     <section className="left__container pt-4 px-2 max-h-[120vh] overflow-y-scroll py-3">
       <div className="user__info flex items-center gap-2">
@@ -32,17 +38,20 @@ const LeftContainer = () => {
           {"New"}
         </Button>
       </div>
-      <div className="repos mt-2 mb-2 flex flex-col gap-2">
-        <Input className="repos__input" placeholder="Find a repository..." />
-        {Array.from({ length: 20 }).map((_, i) => (
+      <div className="repos mt-2 mb-2 flex flex-col gap-2 border border-gray-300 p-2 rounded-[5px] sm:p-0 sm:border-none sm:rounded-none">
+        <Input
+          className="repos__input bg-[#f5f8fb]"
+          placeholder="Find a repository..."
+        />
+        {Array.from({ length: repoCount }).map((_, i) => (
           <Repo key={i} />
         ))}
-        <Repo />
-        <Repo />
-        <Repo />
-        <Repo />
-        <Repo />
-        <Repo />
+        <span
+          className="text-sm text-gray-500 cursor-pointer"
+          onClick={() => setSeeMore(!seeMore)}
+        >
+          {seeMore ? "See less" : "See more"}
+        </span>
       </div>
     </section>
   );
